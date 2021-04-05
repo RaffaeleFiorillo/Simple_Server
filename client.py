e7729a7c681851df2a3a00f9a45cd835bf6e7cd6
RaffaeleFiorillo <raffadeus8@gmail.com>
2021-04-05 19:25:16 -0100

    last update

--- client.py
+++ client.py
@@ -1,20 +1,74 @@
-import webbrowser
+from socket import *
 import sys
+import thread_server
 
-try:
-    ip = sys.argv[1]  # ip of the server
-    port = sys.argv[2]  # port where the server wil respond
-    file_name= sys.argv[3]  # name of the desired file
-    webbrowser.open_new_tab(f"http://{ip}/{file_name}")
-except IndexError:
-    print("Error: Arguments given are not valid")
 
-"""print("Note: file extension is not required when entering the file's name\n")
-print("Welcome")
+# displays the content of the received file on the console
+def show_on_console(file):
+    DIVISION_LINE = "----------------------------------------------------------------"
+    # Shows the Response from server (OK or 404)
+    print(f"\n{DIVISION_LINE}\n# Response from server: {file[0]}{DIVISION_LINE}")
+    print(f"# Content of the file: \n\n{''.join(file[1:-1])}")  # Shows the content of the received file
+    return True
 
-while True:
-    file_name = input('Enter the html file name here: ')
-    if file_name.lower() == "exit":
-        print("See you next time! Bye!")
-        break"""
 
+# sends a message to the server. This message contains the port where the client will receive the data
+def send_connection_request(ip_r, desired_port):
+    tcp_client = socket(AF_INET, SOCK_STREAM)
+    try:
+        # Establish connection to TCP server and exchange data
+        tcp_client.connect((ip_r, 80))
+        tcp_client.sendall(str(desired_port).encode())
+    finally:
+        tcp_client.close()
+
+
+# sends a message to the server asking for a specific file. Then receives the server's response and returns it
+def ask_for_file(ip_r, desired_port, desired_file):
+    tcp_client = socket(AF_INET, SOCK_STREAM)
+    try:
+        # Establish connection to TCP server and exchange data
+        tcp_client.connect((ip_r, desired_port))
+        tcp_client.sendall(f"GET /{desired_file} HTTP/1.1".encode())
+    except IOError:
+        print("Error")
+    files, file = [], ""
+    max_it, current = 1000, 0
+    while file != "END":
+        file = tcp_client.recv(1024).decode()
+        files.append(file)
+        if max_it <= current:
+            files = thread_server.ERROR
+            break
+        current +=1
+    tcp_client.close()
+    return files
+
+
+# establishes the connection with a server on a specified port (handshake) and then asks for a file
+def reaction(ip_r, desired_file, desired_port):
+    send_connection_request(ip_r, desired_port)  # sends, the server, a connection request for a given port
+    file = ask_for_file(ip_r, desired_port, desired_file)  # receive a file from the server
+    return show_on_console(file)  # show the file (line per line) on the console
+
+
+def main():
+    try:
+        ip = sys.argv[1]  # ip of the server
+        port = int(sys.argv[2])  # port where the server wil respond
+        file_name= sys.argv[3]  # name of the desired file
+        return reaction(ip, file_name, port)
+    except IndexError:
+        print("Error: Arguments given are not valid")
+
+
+if __name__ == '__main__':
+    # in case of bad connection, the client makes another request for the file, in this case we don't want the output to
+    # be shown 2 times, so the variable "has_been_shown" indicates if the file should be shown or not (False)
+    has_been_shown = False
+    try:
+        if not has_been_shown:  # verify if file has already been shown
+            has_been_shown = main()  # main will return the correct boolean value of the variable "has_been_shown"
+    except ConnectionResetError:
+        if not has_been_shown:  # if file has never been shown it will be shown now
+            main()
--- thread_server.py
+++ thread_server.py
@@ -1,77 +1,126 @@
 from socket import *
 import threading
+import random
+
+EX_FILES = ["ArqRedes.html", "Example_File_1.html", "Example_File_2.html", "Example_File_3.html"]
+ERROR = ["HTTP/1.1 404 Not Found\r\n\r\n", "<html>\n\r<head>\n\r</head>\n\r\r<body>\n\r\r\r<h1>404 Not Found</h1>\n\r\r"
+         "</body>\n</html>\r\n", "END"]
 
 
-class serverThread(threading.Thread):
-    def __init__(self, server_port):
-        self.last_thread_id = 0
-        threading.Thread.__init__(self)
-        self.server_port = server_port
-        self.serverSocket = socket(AF_INET, SOCK_STREAM)
-        self.serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self.connectionThreads = []
-
-    def run(self):
-        self.serverSocket.bind(('', self.server_port))
-        self.serverSocket.listen(1)
-        while True:
-            print(f"Active Threads: {len(self.connectionThreads)}")
-            # Establish the connection
-            print('Ready to serve...')
-            connectionSocket, addr = self.serverSocket.accept()
-            message = connectionSocket.recv(1024).decode()  # Get message
-            if message == "":
-                print("Ignoring empty message")
-                continue
-            print("Message recieved, opening new thread")
-            self.connectionThreads.append(connectionThread(connectionSocket, message, self.last_thread_id))
-            self.last_thread_id += 1  # increment the id for the next possible thread
-            self.connectionThreads[-1].daemon = 1
-            self.connectionThreads[-1].start()
-            self.close_finished_threads()  # remove all the threads that have done their work
-
-    def close_finished_threads(self):
-        for t in self.connectionThreads:
-            if t.finished:
-                print(f"Thread: {t.t_id} has been closed")
-                self.connectionThreads.remove(t)
+# Creates and returns a new socket and binds it to a given port
+def create_socket(port):
+    new_socket = socket(AF_INET, SOCK_STREAM)
+    new_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    new_socket.bind(('127.8.8.8', int(port)))
+    return new_socket
 
 
-class connectionThread(threading.Thread):
-    def __init__(self, conn_socket, message, t_id):
-        threading.Thread.__init__(self)
-        self.conn_socket = conn_socket
-        self.message = message
-        self.finished = False
-        self.t_id = t_id
-
-    def run(self):
-        try:
-            filename = self.message.split()[1]  # Getting requested HTML page
-            f = open(filename[1:])  # Opening data stream from HTML
-            output_data = f.read()  # Reading HTML page
-            f.close()  # Closing data stream from HTML
-            self.conn_socket.send("HTTP/1.0 200 OK\r\n\r\n".encode())  # Send one HTTP header line into socket
-            for i in range(0, len(output_data)):  # Send the content of the requested file to the client
-                self.conn_socket.send(output_data[i].encode())
-            self.conn_socket.send("\r\n".encode())
-        except IOError:  # Triggered if user requests bad link
-            # Send response message for file not found
-            self.conn_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-            self.conn_socket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
-        finally:
-            self.conn_socket.shutdown(SHUT_RDWR)
-            self.conn_socket.close()
-            self.finished = True
-
-
+# main execution sequence
 def main():
-    server = serverThread(80)  # initialize a server that listens on the port 80
+    server = Main_Thread(80)  # initialize a server that listens on the port 80
     server.daemon = 1  # setting daemon to 1
     server.start()  # start the server
     input()  # to stop execution from ending
-    print("Server execution terminated")  # infrom user that the server has stopped
+    print("\n----- Server execution terminated -----")  # inform user that the server has stopped
     exit()  # end execution of the program
 
 
-main()
+# This class works as a listening server. Every time it receives a new request, it creates a new threads and leads with
+# it. After each request, it cleans the threads that have been closed in order not to occupy to much memory
+class Main_Thread(threading.Thread):
+    def __init__(self, server_port):
+        self.last_thread_id = 0  # represents the id of the last thread, so that every thread has a different id
+        threading.Thread.__init__(self)
+        self.server_port = server_port  # server port where it listens to new requests
+        self.serverSocket = create_socket(self.server_port)  # socket where the Clients make requests
+        self.connectionThreads = []  # list of all the threads
+        self.known_ports = []  # already seen ports
+
+    def run(self):
+        self.serverSocket.listen(1)  # makes the socket listen to Clients
+        dangerous = False  # Identifies if a port is secure or not
+        while True:
+            # Establish the connection
+            print('\n------ Server is listening ------\n')
+            connectionSocket, address = self.serverSocket.accept()  # Receive Requests
+            message = connectionSocket.recv(1024).decode()  # Get message  (with connection port)
+            print(f"\n -> Received request for connection on port: {message} from ip: {address[0]}")
+            if message == "":
+                print("\n -> Ignoring empty message")
+                continue
+            elif message not in self.known_ports:  # this means that a port is not secure
+                dangerous = True
+            self.connectionThreads.append(connectionThread(create_socket(message), self.last_thread_id, dangerous))
+            self.last_thread_id += 1  # increment the id for the next possible thread
+            self.connectionThreads[-1].daemon = 1
+            self.connectionThreads[-1].start()  # start new created thread in order to serve the client on defined port
+            self.close_finished_threads()  # remove all the threads that have done their work
+            print(f"\n -> Connecting with client on port: {message} in a new thread")
+            dangerous = False  # resets the state of the port for the next iteration
+
+    # cleans the thread list from all the threads that have been closed
+    def close_finished_threads(self):
+        opened_threads = []
+        for i in range(len(self.connectionThreads)):
+            if self.connectionThreads[i].finished:
+                print(f" -> Thread: {self.connectionThreads[i].t_id} has been closed")
+                self.connectionThreads[i].join()
+            else:
+                opened_threads.append(self.connectionThreads[i])
+        self.connectionThreads = opened_threads
+
+
+# this class makes a new connection for a client on a specific port, and then he sends a file
+# that the client has requested (in case he finds it, otherwise it sends the 404 code)
+class connectionThread(threading.Thread):
+    def __init__(self, i_socket, t_id, dangerous):
+        threading.Thread.__init__(self)
+        self.thread_socket = i_socket  # the thread socket where the connection will be established
+        self.message = None  # initialize a message
+        self.finished = False  # tells if this thread has finished or not
+        self.t_id = t_id  # id of this thread
+        self.state = dangerous  # tells if it is a dangerous thread
+
+    # in case of exception it sends the 404 error
+    def exception_throw(self, cs, file_name):
+        cs.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        cs.send("<html>\n\r<head>\n\r</head>\n\r\r<body>\n\r\r\r<h1>404 Not Found</h1>\n\r\r"
+                "</body>\n</html>\r\n".encode())
+        cs.send("END".encode())
+        print(f"    --> Client: {self.t_id} has been informed that file: {file_name} have not been found")
+
+    def run(self):
+        self.thread_socket.listen(1)
+        try:
+            connectionSocket, address = self.thread_socket.accept()  # accept connection
+            self.message = connectionSocket.recv(1024).decode()  # Get message
+            filename = self.message.split()[1][1:]  # Getting requested HTML page
+            print(f"    --> Client: {self.t_id} is requesting file: {filename}")
+            if filename not in EX_FILES and self.state:
+                self.exception_throw(connectionSocket, filename)
+            else:
+                f = open(filename)  # Opening data stream from HTML
+                output_data = f.readlines()  # Reading HTML page
+                f.close()  # Closing data stream from HTML
+                connectionSocket.send("HTTP/1.0 200 OK\r\n\r\n".encode())  # Send one HTTP header line into socket
+                for line in output_data:  # Send the content of the requested file to the client
+                    connectionSocket.send(line.encode())
+                connectionSocket.send("\r\n".encode())
+                connectionSocket.send("END".encode())
+                print(f"    --> {filename} has been sent to Client: {self.t_id}")
+        except IOError:  # Triggered if user requests bad link
+            # Send response message for file not found
+            connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+            connectionSocket.send("<html>\n\r<head>\n\r</head>\n\r\r<body>\n\r\r\r<h1>404 Not Found</h1>\n\r\r"
+                                  "</body>\n</html>\r\n".encode())
+            connectionSocket.send("END".encode())
+            print("    --> Client: {self.t_id} has been informed that file: {file_name} have not been found")
+        connectionSocket.shutdown(SHUT_RDWR)
+        connectionSocket.close()
+        self.finished = True
+        print(f"    --> Request for Client: {self.t_id} has been completed")
+        print('\n------ Server is listening ------\n')
+
+
+if __name__ == "__main__":
+    main()
